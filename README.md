4b8f9818c38240ff260910762c0fe602cc6df165
Raymond Ha <raymond@shraymonks.com>
2023-11-05 13:46:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# @shraymonks/eslint-config
+
+My eslint config.
+
+## Installation
+
+```
+pnpm add -D @shraymonks/eslint-config
+```
+
+## Usage
+
+Add to your `.eslintrc.cjs`:
+
+```
+module.exports = {
+  extends: ['@shraymonks'],
+};
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2024: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+};
